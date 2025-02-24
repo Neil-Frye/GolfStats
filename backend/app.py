ab4e335c00ac8b8b652ed43f13e0b3915c369dd7
logicNeo <logicalendar1@gmail.com>
2025-02-24 12:40:58 -0800

    Fix port conflict with AirPlay by changing server port to 8000

--- backend/app.py
+++ backend/app.py
@@ -62,6 +62,6 @@ def server_error(error):
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=int(os.environ.get('PORT', 5000)),
+        port=int(os.environ.get('PORT', 8000)),
         debug=config["app"]["debug"]
     )
